b023512711cf51ed8e44346ae8b3a41d6f3ffd3c
higa <codehiga@gmail.com>
2023-01-10 13:46:01 -0300

    feat: adicionado passphrase ao payload

--- src/services/encrypt-hash-service.ts
+++ src/services/encrypt-hash-service.ts
@@ -1,12 +1,16 @@
 import bcrypt from "bcrypt";
+import { config } from "dotenv";
+config();
 
 export class EncryptHashService {
   static generate(payload: string) {
     const salt = bcrypt.genSaltSync(10);
+    payload = process.env.PASSPHRASE + payload;
     return bcrypt.hashSync(payload, salt);
   }
 
   static isValid(authCode: string, payload: string) {
+    payload = process.env.PASSPHRASE + payload;
     return bcrypt.compareSync(payload, authCode);
   }
 }
