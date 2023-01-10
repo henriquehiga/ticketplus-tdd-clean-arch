67e0b91aecc5ab17d61e9ab40bafb5f01ae1a31b
higa <codehiga@gmail.com>
2023-01-10 14:22:26 -0300

    test: validando documento

---
+++ test/entities/documento.test.ts
@@ -0,0 +1,10 @@
+import { Document } from "@/entities/document";
+
+describe("Documento entity", () => {
+  test("deve criar novo documento válido", () => {
+    let documentoStringValidoEFormatado = "RG/12345678X";
+    const documento = Document.create(documentoStringValidoEFormatado)
+      .value as Document;
+    expect(documento.value).toBe(documentoStringValidoEFormatado);
+  });
+});
