b9dc75be6baa9eed1b43a15c8a337ac37bc1f485
higa <codehiga@gmail.com>
2023-01-10 12:54:00 -0300

    test: entidade ticket criada

---
+++ test/entities/ticket.test.ts
@@ -0,0 +1,34 @@
+import { Ticket } from "@/entities/ticket";
+
+describe("Ticket entity", () => {
+  test("deve retornar valores corretos no get", () => {
+    const id = "abc-123";
+    const authCode = "ABC-123";
+    const payload = {
+      nome: "Client 1",
+      documento: "RG/12345678-X",
+      validade: new Date().toISOString(),
+    };
+    const ticket = new Ticket(id, authCode, payload);
+    expect(ticket.getId()).toBe(id);
+    expect(ticket.getAuthCode()).toBe(authCode);
+    expect(ticket.getPayload()).toEqual(payload);
+  });
+
+  test("deve retornar JSON utilizando metodo toJson", () => {
+    const id = "abc-123";
+    const authCode = "ABC-123";
+    const payload = {
+      nome: "Client 1",
+      documento: "RG/12345678-X",
+      validade: new Date().toISOString(),
+    };
+    const ticket = new Ticket(id, authCode, payload);
+    const expectedJson = {
+      id: id,
+      authCode: authCode,
+      payload: payload,
+    };
+    expect(ticket.toJson()).toEqual(expectedJson);
+  });
+});
