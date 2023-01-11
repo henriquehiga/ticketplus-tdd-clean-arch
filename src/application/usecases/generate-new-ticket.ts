e5f1c6f7e328ced50258eacc804b7bbec6b40afb
higa <codehiga@gmail.com>
2023-01-11 12:16:06 -0300

    feat: id gerado no usecase

--- src/application/usecases/generate-new-ticket.ts
+++ src/application/usecases/generate-new-ticket.ts
@@ -1,13 +1,14 @@
 import { Ticket } from "@/domain/entities/ticket";
 import { TicketPayload } from "../dto/ticket-payload";
 import { AuthenticationService } from "../services/authentication-service";
+import { UuidService } from "../services/uuid-service";
 import { Either, right } from "../shared/either";
 
 export class GenerateNewTicket {
   public async execute(
-    id: string,
     payload: TicketPayload
   ): Promise<Either<void, Ticket>> {
+    const id = UuidService.generate();
     const authCode = AuthenticationService.generate(payload);
     const ticketOrError = Ticket.create(id, authCode, payload);
     if (ticketOrError.isRight()) {
