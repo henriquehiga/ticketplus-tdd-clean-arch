8738c58a98d459c0a2103816ce188f1be518b081
higa <codehiga@gmail.com>
2023-01-11 11:44:24 -0300

    merge

--- src/services/encrypt-hash-service.ts
+++ src/services/encrypt-hash-service.ts
@@ -5,15 +5,17 @@ config();
 
 export class EncryptHashService {
   static generate(payload: string) {
-    const encrypt = CryptoJS.AES.encrypt(payload, process.env.PASSPHRASE);
+    const encrypt = CryptoJS.AES.encrypt(payload, "Oi").toString();
+     this.isValid(encrypt, JSON.parse(payload))
     return encrypt;
   }
 
   static isValid(authCode: string, payload: TicketPayload) {
     const decrypt = CryptoJS.AES.decrypt(
       authCode,
-      process.env.PASSPHRASE
+      "Oi"
     ).toString();
+    console.log(decrypt)
     if (JSON.parse(decrypt).nome == payload.nome) {
       return true;
     }
