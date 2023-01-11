34457c2eed57b528c67de4af531fc935a119c367
higa <codehiga@gmail.com>
2023-01-11 12:16:10 -0300

    test: id gerado no usecase

--- test/usecases/generate-new-ticket.test.ts
+++ test/usecases/generate-new-ticket.test.ts
@@ -1,12 +1,10 @@
 import { TicketPayload } from "@/application/dto/ticket-payload";
 import { AuthenticationService } from "@/application/services/authentication-service";
-import { UuidService } from "@/application/services/uuid-service";
 import { GenerateNewTicket } from "@/application/usecases/generate-new-ticket";
 import { Ticket } from "@/domain/entities/ticket";
 
 describe("GenerateNewTicketUseCase", () => {
   test("deve gerar um ticket integro e único no sistema", async () => {
-    const id = UuidService.generate();
     const payload: TicketPayload = {
       documento: "RG/12345678-X",
       nome: "Cliente Um",
@@ -14,7 +12,7 @@ describe("GenerateNewTicketUseCase", () => {
       usado: false,
     };
     const generateNewTicketUseCase = new GenerateNewTicket();
-    const ticket = (await generateNewTicketUseCase.execute(id, payload))
+    const ticket = (await generateNewTicketUseCase.execute(payload))
       .value as Ticket;
     console.log(ticket);
     const ticketIsValid = AuthenticationService.isValid(ticket.getAuthCode(), ticket.getPayload());
