d773da449c5006cbb0464234344da49a006d52a8
higa <codehiga@gmail.com>
2023-01-11 20:29:34 -0300

    test: novo dto para criacao de ticket

--- test/usecases/generate-new-ticket.test.ts
+++ test/usecases/generate-new-ticket.test.ts
@@ -1,20 +1,19 @@
-import { TicketPayload } from "@/application/dto/ticket-payload";
+import { NewTicketPayload } from "@/application/dto/new-ticket-payload";
 import { AuthenticationService } from "@/application/services/authentication-service";
 import { GenerateNewTicket } from "@/application/usecases/generate-new-ticket";
 import { Ticket } from "@/domain/entities/ticket";
 
 describe("GenerateNewTicketUseCase", () => {
   test("deve gerar um ticket integro e único no sistema", async () => {
-    const payload: TicketPayload = {
+    const payload: NewTicketPayload = {
       documento: "RG/12345678-X",
       nome: "Cliente Um",
-      validade: "2023-12-25T19:00:00.000Z",
-      usado: false,
     };
     const generateNewTicketUseCase = new GenerateNewTicket();
     const ticket = (await generateNewTicketUseCase.execute(payload))
       .value as Ticket;
     const ticketIsValid = AuthenticationService.isValid(ticket.getAuthCode(), ticket.getPayload());
     expect(ticketIsValid).toBeTruthy();
+    console.log(ticket)
   });
 });
