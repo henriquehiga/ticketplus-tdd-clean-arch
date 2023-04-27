2cd74023c5c42882212cbb74a8c98244cf968965
Henrique Higa <codehiga@gmail.com>
2023-04-27 15:27:10 -0300

    feat: implementado validate ticket controller como rota

--- src/application/routes/ticket-routes.ts
+++ src/application/routes/ticket-routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { makeGenerateTicketController } from "../factories/makeGenerateTicketController";
+import { makeValidateTicketController } from "../factories/makeValidateTicketController";
 
 export const ticketRoutes = (router: Router) => {
   router.post("/generate-ticket", async (req, res) => {
     const result = await makeGenerateTicketController().handle(req);
+    res.json(result);
+  });
+
+  router.post("/validate-ticket", async (req, res) => {
+    const result = await makeValidateTicketController().handle(req);
     res.json(result);
   });
 };
