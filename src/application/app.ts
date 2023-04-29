867c926d53ace7a5a21e1b6697974f1b2f174134
Henrique Higa <codehiga@gmail.com>
2023-04-28 22:05:27 -0300

    feat: cors adicionado

--- src/application/app.ts
+++ src/application/app.ts
@@ -1,9 +1,12 @@
+import cors from "cors";
 import express, { json } from "express";
 import { InMemoryTicketRepository } from "./../../test/repository/in-memory-ticket-repository";
 import { configRoutes } from "./config/config-routes";
+
 const app = express();
 
 app.use(json());
+app.use(cors());
 
 export const inMemoryTicketRepo = new InMemoryTicketRepository([]);
 
