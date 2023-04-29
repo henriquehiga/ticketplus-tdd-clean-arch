df89e3ed4bacbd281122b7f7364bde030c9e5326
Henrique Higa <codehiga@gmail.com>
2023-04-28 21:30:42 -0300

    feat: qr-code

---
+++ src/web/src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import QrReader from "react-web-qr-reader";
+
+const App = () => {
+  const delay = 500;
+  const previewStyle = {
+    height: 240,
+    width: 320,
+  };
+
+  const [result, setResult] = useState("No result");
+
+  const handleScan = (result: any) => {
+    console.log(result);
+  };
+
+  const handleError = (error: any) => {
+    console.log(error);
+  };
+
+  return (
+    <>
+      <QrReader
+        delay={delay}
+        style={previewStyle}
+        onError={handleError}
+        onScan={handleScan}
+      />
+      <p>{result}</p>
+    </>
+  );
+};
+
+export default App;
